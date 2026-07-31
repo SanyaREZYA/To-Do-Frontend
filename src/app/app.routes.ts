0715987fb2d2c68396c2e8fd549ba31599fbf3cd
SanyaREZYA <alex07082005m@gmail.com>
2026-07-31 16:00:47 +0300

    add: login and register forms

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
+import { Todo } from './pages/todo/todo';
+import { Register } from './pages/register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'login',
+    component: Login,
+  },
+  {
+    path: 'register',
+    component: Register,
+  },
+  {
+    path: 'todo',
+    component: Todo,
+  },
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: '**',
+    redirectTo: 'login',
+  },
+];
