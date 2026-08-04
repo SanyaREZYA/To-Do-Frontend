a3eca505474bbc18817da821fcfa39b2b6b5873a
SanyaREZYA <alex07082005m@gmail.com>
2026-08-04 18:32:32 +0300

    add: delete item modal

---
+++ src/app/pages/todo/delete-item-modal/delete-item-modal.component.ts
@@ -0,0 +1,25 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-delete-item-modal',
+  imports: [],
+  templateUrl: './delete-item-modal.component.html',
+  styleUrl: './delete-item-modal.component.css',
+})
+export class DeleteItemModalComponent {
+  confirm = output<void>();
+  close = output<void>();
+  cancel = output<void>();
+  itemName = input.required<string>();
+  isTaskListDeleting = input.required<boolean>();
+
+  constructor() {}
+
+  onClose() {
+    this.close.emit();
+  }
+
+  onConfirm() {
+    this.confirm.emit();
+  }
+}
